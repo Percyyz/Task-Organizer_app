d5ebb821b6567d9fc828b690d843ad27580bd9b3
peryz30 <percynoeyarihuaman@gmail.com>
2024-12-01 11:08:03 -0500

    feat: implementando mi pantalla de ajustes

--- Task_Organizer_app/pages/ajustes.py
+++ Task_Organizer_app/pages/ajustes.py
@@ -18,8 +18,50 @@ def ajustes() -> rx.Component:
             rx.text("Aquí puedes configurar los ajustes de la aplicación.",
                 align="center",
                 color="#000000",
-                
+                margin_bottom="20px",
             ),
+            
+            rx.vstack(
+                rx.hstack(
+                    rx.icon(tag="calendar-x", color="#F9A825", size=24),
+                    rx.text("Transferencia automatica de tareas atrasadas", color="#000000", size="md"),
+                    rx.switch(color="#4CAF50"),
+                    align_items="center",
+                ),
+                rx.divider(color="#000000"),
+                rx.hstack(
+                    rx.icon(tag="globe", color="#F9A825", size=24),
+                    rx.text("Idioma", color="#000000", size="md"),
+                    rx.icon(tag="chevron-right", color="#000000"),
+                ),
+                rx.divider(color="#000000"),
+                rx.hstack(
+                    rx.icon(tag="moon", color="#F9A825", size=24),
+                    rx.text("Modo oscuro", color="#000000", size="md"),
+                    rx.switch(color="#4CAF50"),
+                    align_items="center",
+                    justify="space-between",
+                ),
+                rx.divider(color="#000000"),
+                rx.hstack(
+                    rx.icon(tag="bell",color="#F9A825",size=24),
+                    rx.text("Notificacion",color="#000000", size="md"),
+                    rx.icon(tag="chevron-right", color="#000000"),
+                ),
+                rx.divider(color="#000000"),
+                rx.hstack(
+                    rx.icon(tag="cloud-upload",color="#F9A825",size=24),
+                    rx.text("Copia de seguridad",color="#000000", size="md"),
+                    rx.icon(tag="chevron-right", color="#000000"),
+                ),
+                rx.divider(color="#000000"),
+                rx.hstack(
+                    rx.icon(tag="list",color="#F9A825",size=24),
+                    rx.text("categorias",color="#000000", size="md"),
+                    rx.icon(tag="chevron-right", color="#000000"),
+                ),
+            ),
+            
             width="90%",
             max_width="400px",
             background_color="#FFFFFF", 
