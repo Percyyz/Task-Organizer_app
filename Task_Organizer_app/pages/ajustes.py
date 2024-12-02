e142415b603a24718daea85676e6868af7b6cc5e
peryz30 <percynoeyarihuaman@gmail.com>
2024-12-02 10:09:23 -0500

    feat: implementando ajustes

--- Task_Organizer_app/pages/ajustes.py
+++ Task_Organizer_app/pages/ajustes.py
@@ -32,33 +32,61 @@ def ajustes() -> rx.Component:
                 rx.hstack(
                     rx.icon(tag="globe", color="#F9A825", size=24),
                     rx.text("Idioma", color="#000000", size="md"),
-                    rx.icon(tag="chevron-right", color="#000000"),
+                    rx.link(
+                        rx.icon(tag="chevron-right", color="#000000"),
+                        href="",
+                        margin_left="15em"
+                    ),
+                    
                 ),
                 rx.divider(color="#000000"),
                 rx.hstack(
                     rx.icon(tag="moon", color="#F9A825", size=24),
                     rx.text("Modo oscuro", color="#000000", size="md"),
-                    rx.switch(color="#4CAF50"),
-                    align_items="center",
-                    justify="space-between",
+                    rx.color_mode.button(margin_left="11.5em")
+                    # rx.segmented_control.root(
+                    #     rx.segmented_control.item(
+                    #     rx.icon(tag="sun", size=20),
+                    #     value="light",
+                    #     ),
+                    #     rx.segmented_control.item(
+                    #     rx.icon(tag="moon", size=20),
+                    #     value="dark",
+                    #     ),
+                    # )
+                    
+                    # rx.switch(color="#4CAF50",margin_left="11.7em"),
+                    # align_items="center",
                 ),
                 rx.divider(color="#000000"),
                 rx.hstack(
                     rx.icon(tag="bell",color="#F9A825",size=24),
                     rx.text("Notificacion",color="#000000", size="md"),
-                    rx.icon(tag="chevron-right", color="#000000"),
+                    rx.link(
+                        rx.icon(tag="chevron-right", color="#000000"),
+                        href="",
+                        margin_left="12.5em"
+                    )
                 ),
                 rx.divider(color="#000000"),
                 rx.hstack(
                     rx.icon(tag="cloud-upload",color="#F9A825",size=24),
                     rx.text("Copia de seguridad",color="#000000", size="md"),
-                    rx.icon(tag="chevron-right", color="#000000"),
+                    rx.link(
+                        rx.icon(tag="chevron-right", color="#000000"),
+                        href="",
+                        margin_left="9.2em"
+                    )
                 ),
                 rx.divider(color="#000000"),
                 rx.hstack(
                     rx.icon(tag="list",color="#F9A825",size=24),
                     rx.text("categorias",color="#000000", size="md"),
-                    rx.icon(tag="chevron-right", color="#000000"),
+                    rx.link(
+                        rx.icon(tag="chevron-right", color="#000000"),
+                        href="",
+                        margin_left="13.2em"
+                    )
                 ),
             ),
             
